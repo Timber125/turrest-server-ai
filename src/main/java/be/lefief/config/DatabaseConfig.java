7cf76a3ce559d51120196488835e9e66645a21a4
Timber Lefief <timberlefief@gmail.com>
2025-09-07 23:22:46 +0200

    Fix h2 setup

--- src/main/java/be/lefief/config/DatabaseConfig.java
+++ src/main/java/be/lefief/config/DatabaseConfig.java
@@ -1,8 +1,41 @@
 package be.lefief.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties mainDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "mainDataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource mainDataSource() {
+        return mainDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "jdbcmain")
+    public JdbcTemplate mainJdbcTemplate(@Qualifier("mainDataSource") DataSource mainDataSource) {
+        return new JdbcTemplate(mainDataSource);
+    }
+
+    @Bean(name = "namedParameterJdbcTemplate")
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(@Qualifier("mainDataSource") DataSource mainDataSource) {
+        return new NamedParameterJdbcTemplate(mainDataSource);
+    }
 
 }
